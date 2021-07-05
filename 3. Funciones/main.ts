4480876abb9e7d11d8a9a0cb9f9918403189da43
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-07-04 21:38:41 -0400

    Funciones, como indicar tipos por los parametros

---
+++ 3. Funciones/main.ts
@@ -0,0 +1,30 @@
+// ╔══════════════╗
+
+// 3. FUNCIONES
+
+// ╚══════════════╝
+// 3.1 Parametros
+// ---------------------------------------
+// Para los parametros se hace lo mismo que las variables, se les indica el tipo con:
+// EJ: (variable:tipo)
+
+/*
+	const getFullname = (name: string, surname: string) => {
+		return `${name} ${surname}`;
+	};
+
+	console.log(getFullname('Jose', 'Sanchez'));
+*/
+
+// 3.2 Especificando que retornara
+// ---------------------------------------
+// Despues de los parametros de la funcion se coloca el tipo de valor que retornara nuestra funcion
+// EJ: const funcion = (parametros):tipo => {}
+
+/*
+	const getFullname = (name: string, surname: string): string => {
+		return `${name} ${surname}`;
+	};
+
+	console.log(getFullname('Jose', 'Sanchez'));
+*/
