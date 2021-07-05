00c989ed285927827e2f3f84905e7ba209e0335a
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-07-04 21:37:41 -0400

    Variables con TypeScript

---
+++ 2. Variables/main.js
@@ -0,0 +1,7 @@
+// ╔══════════════╗
+// 2. VARIABLES
+// ╚══════════════╝
+// Al transpilarse, todas las variables se transformaran a var debido a que se transpila a EcmaScript 3
+// Las variables se indican el tipo asi:
+// variable:tipo = valor
+/* let hello: string = 'xd'; */
---
+++ 2. Variables/main.ts
@@ -0,0 +1,11 @@
+// ╔══════════════╗
+
+// 2. VARIABLES
+
+// ╚══════════════╝
+
+// Al transpilarse, todas las variables se transformaran a var debido a que se transpila a EcmaScript 3
+// Las variables se indican el tipo asi:
+// variable:tipo = valor
+
+/* let hello: string = 'xd'; */
